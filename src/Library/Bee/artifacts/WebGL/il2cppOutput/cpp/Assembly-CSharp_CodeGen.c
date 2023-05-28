914347d2e0da65bbe4c3b73f71e78fa9577ae9d9
sG468 <ryuou4297@gmail.com>
2023-05-28 17:29:13 +0900

    commit

---
+++ src/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,93 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BoardController::ClearAll()
+extern void BoardController_ClearAll_m823D53547FE172717EC21B2B81B5AF4ECFE3D05F (void);
+// 0x00000002 System.Void BoardController::Start()
+extern void BoardController_Start_m210233860A3659110B44786BF1CE10CA94AB8534 (void);
+// 0x00000003 System.Boolean BoardController::IsValidated(UnityEngine.Vector2Int)
+extern void BoardController_IsValidated_mBEEC9CD7B88E8DD421834DE38094248CCB2B7697 (void);
+// 0x00000004 System.Boolean BoardController::CanSettle(UnityEngine.Vector2Int)
+extern void BoardController_CanSettle_m2480A0489D87E9F7BC18379ECA7F69F7446E726B (void);
+// 0x00000005 System.Boolean BoardController::Settle(UnityEngine.Vector2Int,System.Int32)
+extern void BoardController_Settle_mD1340D08A11664F9E2A07E75D6059D61C3CFAD52 (void);
+// 0x00000006 System.Void BoardController::.ctor()
+extern void BoardController__ctor_m479846F2B44642921ACB4BA2F4ADB8A7E88BCC58 (void);
+// 0x00000007 System.Void PuyoController::SetPuyoType(PuyoType)
+extern void PuyoController_SetPuyoType_m6E8C682F8FB03447ED84D3DC1C10B10D2B7E57C6 (void);
+// 0x00000008 PuyoType PuyoController::GetPuyoType()
+extern void PuyoController_GetPuyoType_mCA908DEEA944FD0153966E2975366868C1DBD3D7 (void);
+// 0x00000009 System.Void PuyoController::SetPos(UnityEngine.Vector3)
+extern void PuyoController_SetPos_mE011A2D0894FFD0AC9DE262254D8FC44C8E28080 (void);
+// 0x0000000A System.Void PuyoController::.ctor()
+extern void PuyoController__ctor_m85BE1D31D71D52118CEC2382EB3142E7B9AC65B7 (void);
+// 0x0000000B System.Void PuyoController::.cctor()
+extern void PuyoController__cctor_m009D1F76FE9A470867806AF73E4B7D2A926CE8C1 (void);
+// 0x0000000C System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x0000000D System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	BoardController_ClearAll_m823D53547FE172717EC21B2B81B5AF4ECFE3D05F,
+	BoardController_Start_m210233860A3659110B44786BF1CE10CA94AB8534,
+	BoardController_IsValidated_mBEEC9CD7B88E8DD421834DE38094248CCB2B7697,
+	BoardController_CanSettle_m2480A0489D87E9F7BC18379ECA7F69F7446E726B,
+	BoardController_Settle_mD1340D08A11664F9E2A07E75D6059D61C3CFAD52,
+	BoardController__ctor_m479846F2B44642921ACB4BA2F4ADB8A7E88BCC58,
+	PuyoController_SetPuyoType_m6E8C682F8FB03447ED84D3DC1C10B10D2B7E57C6,
+	PuyoController_GetPuyoType_mCA908DEEA944FD0153966E2975366868C1DBD3D7,
+	PuyoController_SetPos_mE011A2D0894FFD0AC9DE262254D8FC44C8E28080,
+	PuyoController__ctor_m85BE1D31D71D52118CEC2382EB3142E7B9AC65B7,
+	PuyoController__cctor_m009D1F76FE9A470867806AF73E4B7D2A926CE8C1,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	5070,
+	5070,
+	7519,
+	3025,
+	1474,
+	5070,
+	4047,
+	4933,
+	4169,
+	5070,
+	7978,
+	5070,
+	5070,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
